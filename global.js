de2011c6df414ab6a83038a58bc326fcc0c65e88
muniquefeitoz4@gmail.com <muniquefeitoz4@gmail.com>
2023-12-12 13:15:17 -0300

    add a global css and js

---
+++ global.js
@@ -0,0 +1,11 @@
+const responsive_wrapper = document.querySelector(".responsive_wrapper");
+const responsive_wrapper_btn = document.querySelector(".responsive_wrapper_btn");
+const app_navigation_menu = document.querySelector(".app_navigation_menu");
+
+responsive_wrapper_btn.addEventListener("click", () => {
+  responsive_wrapper.style.display = "none";
+});
+
+app_navigation_menu.addEventListener("click", () => {
+  
+});
