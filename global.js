1fb1f69328008ee67e2e2f39cad93e75d1dbe1a0
muniquefeitoz4@gmail.com <muniquefeitoz4@gmail.com>
2023-12-13 11:18:30 -0300

    adicionando o menu retrátil

--- global.js
+++ global.js
@@ -1,5 +1,6 @@
 const responsive_wrapper = document.querySelector(".responsive_wrapper");
 const responsive_wrapper_btn = document.querySelector(".responsive_wrapper_btn");
+const responsive_app_navigation_menu = document.querySelector(".responsive_app_navigation_menu");
 const app_navigation_menu = document.querySelector(".app_navigation_menu");
 
 responsive_wrapper_btn.addEventListener("click", () => {
@@ -7,5 +8,5 @@ responsive_wrapper_btn.addEventListener("click", () => {
 });
 
 app_navigation_menu.addEventListener("click", () => {
-  
+  responsive_app_navigation_menu.classList.toggle("app_navigation_menu_toggle");
 });
